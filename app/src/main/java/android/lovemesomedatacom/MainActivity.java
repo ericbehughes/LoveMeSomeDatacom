4d866f0c0deaad37b9641600acab3adee4c1b902
ericbehughes <e.hughes92@icloud.com>
2017-11-17 15:20:30 -0500

    started base view and added ClassCancelationActivity

--- app/src/main/java/android/lovemesomedatacom/MainActivity.java
+++ app/src/main/java/android/lovemesomedatacom/MainActivity.java
@@ -1,13 +1,27 @@
 package android.lovemesomedatacom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void classCancellationClick(View view) {
+
+        Intent aboutIntent = new Intent(this, ClassCancelationActivity.class);
+
+        Log.d(TAG, "classCancellationClick");
+        startActivity(aboutIntent);
     }
 }
