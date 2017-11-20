8e6066f3f9afbc6e56964190ad27d792eb14b866
ericbehughes <e.hughes92@icloud.com>
2017-11-20 14:17:46 -0500

    added sharedpreferences key enum for settings

---
+++ app/src/main/java/android/lovemesomedatacom/SharedPreferencesKey.java
@@ -0,0 +1,23 @@
+package android.lovemesomedatacom;
+import android.content.SharedPreferences;
+
+
+enum SharedPreferencesKey {
+    
+    FIRST_NAME("FIRST_NAME"),
+    LAST_NAME("LAST_NAME"),
+    EMAIL_ADDRESS("EMAIL_ADDRESS"),
+    PASSWWORD("PASSWORD"),
+    DATE_STAMP("DATE_STAMP");
+
+    private String key;
+
+    SharedPreferencesKey(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
