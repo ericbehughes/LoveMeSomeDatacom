1c51e07e546e4520beefa0812c644af1b3f86ba1
ericbehughes <e.hughes92@icloud.com>
2017-11-20 20:13:45 -0500

    added ClassCancelation fragment list view from fragment example

--- app/src/main/java/android/lovemesomedatacom/ClassCancelationActivity.java
+++ app/src/main/java/android/lovemesomedatacom/ClassCancelationActivity.java
@@ -1,16 +1,88 @@
 package android.lovemesomedatacom;
 
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-public class ClassCancelationActivity extends MenuActivity {
+public class ClassCancelationActivity extends AppCompatActivity  implements ClassCancelationsFragment.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_cancellation);
+        ClassCancelationsFragment firstFragment;
+        FragmentTransaction ft =
+                getSupportFragmentManager().beginTransaction();// begin  FragmentTransaction
+        firstFragment = new ClassCancelationsFragment();
 
-        Toast.makeText(getApplicationContext(), "ClassCancelationActivity:Success", Toast.LENGTH_SHORT).show();
+        Log.d("DEBUG", getResources().getConfiguration().orientation + "");
+
+        if (savedInstanceState == null) {
+            // Instance of first fragment
+            // Add Fragment to FrameLayout (flContainer), using FragmentManager
+            ft.add(R.id.flContainer, firstFragment);                                // add    Fragment
+            ft.commit();                                                            // commit FragmentTransaction
+        } else {
+            // if this is not done, the menu fragment ended up duplicate
+            //  on rotate back to landscape, only when the menu activity was
+            //  last active
+            ft.replace(R.id.flContainer, firstFragment);                                // add    Fragment
+            ft.commit();                                                            // commit FragmentTransaction
+        }
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            ClassCancelationsFragment secondFragment = new ClassCancelationsFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", 0);
+            secondFragment.setArguments(args);          // (1) Communicate with Fragment using Bundle
+            FragmentTransaction ft2 = getSupportFragmentManager().beginTransaction();// begin  FragmentTransaction
+            ft2.add(R.id.flContainer, secondFragment);                               // add    Fragment
+            ft2.commit();                                                            // commit FragmentTransaction
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "main Activity onStop ", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClassCancellationSelected(int position) {
+        Toast.makeText(this, "Called By Fragment A: position - "+ position, Toast.LENGTH_SHORT).show();
+
+        // Load Pizza Detail Fragment
+        ClassCancelationDetailFragment secondFragment = new ClassCancelationDetailFragment();
+
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        secondFragment.setArguments(args);          // (1) Communicate with Fragment using Bundle
+
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flContainer, secondFragment) // replace flContainer
+                    //.addToBackStack(null)
+                    .commit();
+        }else{
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flContainer, secondFragment) // replace flContainer
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/android/lovemesomedatacom/ClassCancelationModel.java
@@ -0,0 +1,21 @@
+package android.lovemesomedatacom;
+
+/**
+ * Created by ehugh on 11/20/2017.
+ */
+
+public class ClassCancelationModel {
+
+
+    public static String[] cancelationMenu = {
+            "Java",
+            "Android1",
+            "PHP"
+    };
+
+    public static String[] cancelationDetails = {
+            "Java Description",
+            "Android Description",
+            "Php description"
+    };
+}
