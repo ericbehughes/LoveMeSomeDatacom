a03ced88669148ca1cd23fb278772fe81621de3b
Sebastian Ramirez <sebastian.ramirez.p@outlook.com>
2017-12-06 12:25:50 -0500

    Work on AddToCalendar module FIRST DRAFT (Working with Eric) Work on AndroidManifest.xml 	Change indentation. Work on CalendarActivity.java 	Remove intent call for Calendar app 	Now adds the event directly to the calendar without having to go 	through the app (as specified in the specs). 	TODO: Add appropriate widgets and validation. Rename TemperatureLocation.java 	Was called TemperatureLoaction.java Work on activity_calendar.xml 	Give meaningful id to the search button. Add string for toast in the CalendarAvtivity.

--- app/src/main/java/android/lovemesomedatacom/CalendarActivity.java
+++ app/src/main/java/android/lovemesomedatacom/CalendarActivity.java
@@ -7,20 +7,24 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.icu.util.TimeZone;
 import android.net.Uri;
 import android.nfc.Tag;
 import android.provider.CalendarContract;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -30,9 +34,9 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-public class CalendarActivity extends MenuActivity implements TimePickerFragment.OnCompleteListener  {
+public class CalendarActivity extends MenuActivity implements TimePickerFragment.OnCompleteListener {
 
-    private static final String TAG = "Calendar Activity";
+    private static final String TAG = CalendarActivity.class.getSimpleName();
     Calendar beginTime = Calendar.getInstance();
 
     private TextView tvEventName;
@@ -45,7 +49,9 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
     private TextView tvDateValue;
     private int currentTimeElement;
     private long pickedDateMilis;
-
+    private final String CALENDAR_PERMISSION = Manifest.permission.WRITE_CALENDAR;
+    private final int PERMISSION_CODE = 1;
+    private boolean isPermissionGranted;
 
     /**
      * Find the Views in the layout<br />
@@ -62,6 +68,7 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
         tvEndTimeValue = (TextView) findViewById(R.id.tvEndTimeValue);
         tvDate = (TextView) findViewById(R.id.tvDate);
         tvDateValue = (TextView) findViewById(R.id.tvDateValue);
+
     }
 
 
@@ -73,6 +80,23 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
         this.setTitle(R.string.add_to_calendar_activity_title);
         final Calendar myCalendar = Calendar.getInstance();
 
+        if (ContextCompat.checkSelfPermission(this,
+                CALENDAR_PERMISSION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    CALENDAR_PERMISSION)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{CALENDAR_PERMISSION},
+                        2);
+
+            }
+        }
+
         final DatePickerDialog.OnDateSetListener datePickerListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
@@ -80,12 +104,11 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                //pickedDateMilis = myCalendar.getTimeInMillis();
                 myCalendar.set(Calendar.HOUR_OF_DAY, 0);
                 myCalendar.set(Calendar.MINUTE, 0);
                 myCalendar.set(Calendar.SECOND, 0);
                 myCalendar.set(Calendar.MILLISECOND, 0);
-                pickedDateMilis =myCalendar.getTimeInMillis();
+                pickedDateMilis = myCalendar.getTimeInMillis();
                 Log.d(TAG, "CURRENT_DATE_ANON_CLASS: " + pickedDateMilis);
                 String myFormat = "dd/MM/yy"; //In which you need put here
                 SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.getDefault());
@@ -98,63 +121,46 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
             @Override
             public void onClick(View view) {
                 new DatePickerDialog(CalendarActivity.this, datePickerListener, myCalendar
-                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH ),
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
 
             }
         });
     }
+//    }
 
 
     public void showTimePickerDialog(View v) {
         currentTimeElement = v.getId();
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
-        
+
     }
 
     public void addToCalendar(View view) {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            Intent intent = new Intent(Intent.ACTION_INSERT);
-            intent.setType("vnd.android.cursor.item/event");
-
-            String[] array = tvStartTimeValue.getText().toString().split(":");
-
-            long temp = Long.parseLong(array[0]);
-            long minutes = Long.parseLong(array[1]);
-            if (temp > 12){
-                temp = temp - 12;
-            }
-            long startTimeHours = TimeUnit.HOURS.toMillis(10);
-            long startTimeMinutes = TimeUnit.MINUTES.toMillis(minutes);
-
-            //16:40
-            // have to convert string 16:43 into milliseconds
-            intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, pickedDateMilis );
-            intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,pickedDateMilis);
-            intent.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false);
-//            intent.putExtra(CalendarContract.Events.DTSTART, currentDateMilis);
-//            Log.d(TAG, "CURRENT_DATE_ADD_TO_CALENDAR: " + currentDateMilis);
-//            intent.putExtra(CalendarContract.Events.DTEND, currentDateMilis);
-            intent.putExtra(CalendarContract.Events.TITLE, "Event title goes here");
-            intent.putExtra(CalendarContract.Events.DESCRIPTION,  "Event Description");
-            intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Location");
-            intent.putExtra(CalendarContract.Events.RRULE, "FREQ=YEARLY");
-
-            startActivity(intent);
-
-            // get the event ID that is the last element in the Uri
-            //long eventID = Long.parseLong(uri.getLastPathSegment());
-            return;
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                CALENDAR_PERMISSION)
+                == PackageManager.PERMISSION_GRANTED) {
+            ContentResolver contentResolver = getContentResolver();
+            long calID = 3;
+            ContentValues values = new ContentValues();
+            values.put(CalendarContract.Events.DTSTART, pickedDateMilis);
+            values.put(CalendarContract.Events.DTEND, pickedDateMilis + 60000);
+            values.put(CalendarContract.Events.TITLE, tvEventName.getText().toString());
+            values.put(CalendarContract.Events.CALENDAR_ID, calID);
+            TimeZone tz = TimeZone.getDefault();
+            values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().toString());
+            Uri uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
+        } else {
+            Toast.makeText(this, R.string.permission_toast, Toast.LENGTH_LONG).show();
         }
-
 
     }
 
     @Override
     public void onComplete(String time) {
-        switch(currentTimeElement){
+        switch (currentTimeElement) {
 
             case R.id.tvStartTimeValue:
                 Log.d(TAG, "startTimeValue was clicked");
@@ -168,12 +174,3 @@ public class CalendarActivity extends MenuActivity implements TimePickerFragment
         }
     }
 }
-
-
-
-
-
-
-
-
-
